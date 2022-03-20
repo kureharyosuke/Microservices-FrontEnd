ed88e91f4d72bce0dd06d7a41f6ebaf58b2819e7
kureharyosuke <ohhayo.com@gmail.com>
2022-03-20 17:16:37 +0900

    #1 add: app.ts import express, cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,15 @@
-console.log("hello typescipt");
+import * as express from "express";
+import * as cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://localhost:8080", "http://localhost:4200"],
+  })
+);
+
+app.use(express.json());
+
+console.log("✅ Listening on port: 8000");
+app.listen(8000);
