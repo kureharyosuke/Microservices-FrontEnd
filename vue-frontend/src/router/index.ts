c48622d0222f1f894d1e1397428fae89f01625b0
kureharyosuke <ohhayo.com@gmail.com>
2022-03-21 21:47:16 +0900

    #7 add: Products.vue is Router setup

--- vue-frontend/src/router/index.ts
+++ vue-frontend/src/router/index.ts
@@ -1,19 +1,21 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+// import Admin from "@/pages/admin/Admin.vue";
+//
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView,
+    path: "/",
+    name: "admin",
+    component: () => import("@/pages/Admin.vue"),
+    children: [{ path: "/", component: () => import("@/pages/admin/Products.vue") }],
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
