1a732b2606c68c5aa94a8e85d45a858ed43887aa
kureharyosuke <ohhayo.com@gmail.com>
2022-03-21 18:44:57 +0900

    #7 add: component Nav & router add admin

--- vue-frontend/src/pages/admin/Admin.vue
+++ vue-frontend/src/pages/admin/Admin.vue
@@ -1,30 +1,14 @@
 <template>
-  <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-    <input
-      class="form-control form-control-dark w-100"
-      type="text"
-      placeholder="Search"
-      aria-label="Search"
-    />
-    <ul class="navbar-nav px-3">
-      <li class="nav-item text-nowrap">
-        <a class="nav-link" href="#">Sign out</a>
-      </li>
-    </ul>
-  </nav>
-
   <div class="container-fluid">
     <div class="row">
       <nav class="col-md-2 d-none d-md-block bg-light sidebar">
         <div class="sidebar-sticky">
           <ul class="nav flex-column">
             <li class="nav-item">
-              <a class="nav-link active" href="#">
-                Products
-              </a>
+              <a class="nav-link active" href="#"> Products </a>
             </li>
           </ul>
+        </div>
       </nav>
 
       <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
